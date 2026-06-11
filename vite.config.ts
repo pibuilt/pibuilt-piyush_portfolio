b6b461a016146945f44a607469039988aa4ed6ca
Piyush Bhuyan <mrpiyush0007@gmail.com>
2026-06-11 21:25:37 +0530

    fix: url path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/piyush_portfolio/',
+  base: '/pibuilt-piyush_portfolio/',
   plugins: [react()],
 });
-
